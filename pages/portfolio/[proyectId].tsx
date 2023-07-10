b96564516d8c649c0ff096c0d03ef86d09b113b3
martin <martin.alvarado2002@gmail.com>
2023-07-10 12:43:17 +0200

    [#3-61]: Extracting Dynamic Path Segment Data (Dynamic Routes)

--- pages/portfolio/[proyectId].tsx
+++ pages/portfolio/[proyectId].tsx
@@ -1,4 +1,11 @@
+import { useRouter } from 'next/router';
+
 export default function PortfolioProyectPage() {
+  const router = useRouter();
+
+  console.log(`🔎 | PortfolioProyectPage | pathName:`, router.pathname);
+  console.log(`🔎 | PortfolioProyectPage | query:`, router.query);
+
   return (
     <div>
       <h1>The portfolio Proyect Page</h1>
