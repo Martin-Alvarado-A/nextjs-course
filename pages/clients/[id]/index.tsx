51492ba602b556dcb1ba036870ca3d086e44bda6
martin <martin.alvarado2002@gmail.com>
2023-07-13 13:01:56 +0200

    [#3-67]: Navigating Programmatically

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -2,9 +2,19 @@ import { useRouter } from 'next/router';
 export default function CliensProjectPage() {
   const Router = useRouter();
   console.log(`🔎 | CliensProjectPage | query:`, Router.query);
+
+  function loadProjectHandler() {
+    //...load data
+    Router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: Router.query.id, clientprojectid: 'projecttest' },
+    });
+  }
+
   return (
     <div>
       <h1>The projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load project Test</button>
     </div>
   );
 }
