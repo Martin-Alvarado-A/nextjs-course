9ca7b10d4bed20df2ee25ca7dc6aa6b75241a587
martin <martin.alvarado2002@gmail.com>
2023-09-04 16:03:53 +0200

    [#4-82]: Working on the "All Events" Page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,7 +1,12 @@
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/EventList';
+
 export default function AllEventsPage() {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>The events Page</h1>
+      <EventList items={events} />
     </div>
   );
 }
