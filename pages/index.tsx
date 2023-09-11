19fe5ab1a41c8272f3411d9924aebfd1a99afe0d
martin <martin.alvarado2002@gmail.com>
2023-09-11 14:25:40 +0200

    [#5-99]: A Closer Look At "getStaticProps" & Configuration Options

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,11 +13,24 @@ export default function HomePage(props: any) {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context: any) {
   console.log(`🔎 | Index | getStaticProps > Re-Generating...`);
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData.toString());
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
   return {
     props: {
       products: data.products,
